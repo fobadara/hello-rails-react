f645bae52f72e7a17d3dd55521618495c2a6796e
fobadara <fobadara@gmail.com>
2022-06-16 22:02:46 +0100

    Fix trailing comma error

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = (api) => {
     throw new Error(
       `${'Please specify a valid `NODE_ENV` or '
       + '`BABEL_ENV` environment variables. Valid values are "development", '
-      + '"test", and "production". Instead, received: '}${JSON.stringify(currentEnv)}.`
+      + '"test", and "production". Instead, received: '}${JSON.stringify(currentEnv)}.`,
     );
   }
 
